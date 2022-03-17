20b301159c24f831273cb3ccf42e29e3a9050d2a
ganeshmev <35722311+ganeshmev@users.noreply.github.com>
2022-03-17 09:51:05 +0530

    Made for MKS GenL V2.1
    Made for MKS GenL V2.1 testing it.

--- src/Marlin/_Version.h
+++ src/Marlin/_Version.h
@@ -8,14 +8,14 @@
 #define REQUIRED_CONFIGURATION_ADV_H_VERSION 010109
 
 #define PROTOCOL_VERSION "1.0"
-#define MACHINE_NAME "Julia Advanced ABL with TMC"
-#define DEFAULT_MACHINE_UUID "b68ca0d9-60d2-445c-b234-e90f8ad69a08"
+#define MACHINE_NAME "Julia Advance with TMC 2208 on MKS GenL V2.1, Not using UART- Top right 2 jumpers jumped horizontally, 1/16 micro stepping"
+#define DEFAULT_MACHINE_UUID "0108a599-3b95-4186-a636-069eb59879c0"
 
-#define BUILD_UNIX_DATETIME "1598253632"
-#define STRING_DISTRIBUTION_DATE "2030-08-24 12:50"
+#define BUILD_UNIX_DATETIME "1646032316"
+#define STRING_DISTRIBUTION_DATE "2022-02-28 12:51"
 
-#define SHORT_BUILD_VERSION "J18RX"
-#define DETAILED_BUILD_VERSION "J18RX_04062021_13:30_TMC"
+#define SHORT_BUILD_VERSION "JA20"
+#define DETAILED_BUILD_VERSION "JA20_220228_1251"
 
-#define SOURCE_CODE_URL "https://github.com/ganeshmev/Julia2018Marlin119-Advanced"
+#define SOURCE_CODE_URL "https://github.com/FracktalWorks/Julia2018Marlin119"
 #define WEBSITE_URL "https://fracktal.in"
